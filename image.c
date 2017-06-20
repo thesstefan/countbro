af20bbf07fffd03f8439e1d415e8dfa30366cad8
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-06-21 02:30:34 +0300

    Updated

--- image.c
+++ image.c
@@ -11,13 +11,13 @@ void _read_pixels(int heigth, int width, struct PIXEL *pixels, FILE *input) {
     }
 }
 
-void _write_pixel(int heigth, int width, struct PIXEL *pixels, FILE *output) {
+void _write_pixels(int heigth, int width, struct PIXEL *pixels, FILE *output) {
     int subpixels_per_line = width * 3;
     int required_padding = (subpixels_per_line % 4) ? 4 - subpixels_per_line % 4 : 0;
     int padding_dump = 0;
 
     for (int heigth_index = heigth - 1; heigth_index >= 0; heigth_index--) {
-        fwrite(pixels + heigth_index * width, sizoef(struct PIXEL), width, output);
+        fwrite(pixels + heigth_index * width, sizeof(struct PIXEL), width, output);
         fwrite(&padding_dump, 1, required_padding, output);
     }
 }
@@ -26,11 +26,11 @@ struct Image *_read_image(FILE *input) {
     struct Image *image = malloc(sizeof(struct Image));
 
     fread(&(image->file_header), 1, sizeof(struct FILE_HEADER), input);
-    fread(&(image->image_header), 1, sizeof(struct IMAGE_HEADER, input);
+    fread(&(image->image_header), 1, sizeof(struct IMAGE_HEADER), input);
 
     image->pixels = malloc(image->image_header.heigth * image->image_header.width * sizeof(struct PIXEL));
 
-    fseek(input, image->file_header.image.data.offset, SEEK_SET);
+    fseek(input, image->file_header.image_data_offset, SEEK_SET);
 
     _read_pixels(image->image_header.heigth, image->image_header.width, image->pixels, input);
 
@@ -51,14 +51,14 @@ void _write_image(struct Image *image, FILE *output) {
 }
 
 struct Image *read_image_from_file(char *filename) {
-    FILE *input = fopen(filenamae, "rb");
+    FILE *input = fopen(filename, "rb");
     struct Image *image = _read_image(input);
     fclose(input);
 
     return image;
 }
 
-struct Image *write_image_to_file(struct Image *image, char *filename) {
+void write_image_to_file(struct Image *image, char *filename) {
     FILE *output = fopen(filename, "wb");
     _write_image(image, output);
     fclose(output);
