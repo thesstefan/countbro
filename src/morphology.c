782e6272f15e1d10ab657f66ae1bdba5412c1165
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-06-29 20:39:23 +0300

    Refactor

--- src/morphology.c
+++ src/morphology.c
@@ -1,18 +1,23 @@
 #include "colors.h"
 #include "morphology.h"
 
-enum kernel_state check_kernel(binary_image image, int width, int height, struct Kernel *kernel, int x, int y) {
+enum kernel_state _check_kernel(struct binary_image *image, struct Kernel *kernel, int x, int y) {
     int kernel_state_count = 0;
-    int index = 0;
-    int leap = kernel->width / 2;
 
-    for (int y_index = y - leap; y_index >= 0 && y_index <= y + leap && y_index < height; y_index++)
-        for (int x_index = x - leap; x_index >= 0 && x_index <= x + leap && x_index < width; x_index++) {
-            if (kernel->kernel[index] == image[index])
-                kernel_state_count++;
+    int kernel_width_index = 0;
+    int kernel_height_index = 0;
 
-            index++;
+    for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
+        for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
+            if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == *(image->matrix + height * image->width + width))
+               kernel_state_count++;
+
+            kernel_width_index++;
         }
+        
+        kernel_width_index = 0;
+        kernel_height_index++;
+    }
 
     if (kernel_state_count == kernel->width * kernel->height)
         return FIT;
@@ -38,51 +43,91 @@ void delete_kernel(struct Kernel *kernel) {
     free(kernel);
 }
 
-// Using only odd square_size values
-void fill_square(binary_image image, int width, int height,  int square_size, enum binary_color new_color, int x, int y) {
-    for (int y_index = y - 1; y_index <= y + 1; y_index++)
-        for (int x_index = x - 1; x_index <= x + 1; x_index++)
-            image[y_index * width + x_index] = new_color;
-}
+struct binary_image *_copy_binary(struct binary_image *image) {
+    struct binary_image *copy = malloc(sizeof(struct binary_image));
 
+    copy->height = image->height;
+    copy->width = image->width;
 
-/*
-void binary_image erosion(binary_image image, struct Kernel *kernel) {
-    binary_image *image_copy = malloc(height * width * sizeof(binary_image));
+    copy->matrix = malloc(copy->height * copy->width * sizeof(enum binary_color));
 
-    for (int height_index = 0; height_index < height; height_index++)
-        for (int width_index = 0; width_index < width; width_index++)
-            if (image[height_index * width + width_index] == WHITE)
-                if (check_kernel(image_copy, kernel, height_index, width_index) == FIT) {
-                    for (int 
-
-}
-*/
-
-void dilation(binary_image image, int height, int width, struct Kernel *kernel) {
-    binary_image image_copy = malloc(height * width * sizeof(enum binary_color));
-    int leap = kernel->width / 2;
-
-    for (int height_index = 0; height_index < height; height_index++)
-        for (int width_index = 0; width_index < width; width_index++) {
-            if (image_copy[height_index * width + width_index] == WHITE)
-                if (check_kernel(image_copy, width, height, kernel, width_index, height_index) != HIT)
-                    for (int y_index = height_index - leap; y_index >= 0 && y_index < height && y_index <= height_index + leap; y_index++) 
-                        for (int x_index = width_index - leap; x_index >= 0 && x_index < width && x_index <= width_index + leap; x_index++) 
-                            image[y_index * width + x_index] = WHITE;
+    for (int height = 0; height < copy->height; height++) {
+        for (int width = 0; width < copy->width; width++) {
+            *(copy->matrix + height * copy->width + width) = *(image->matrix + height * image->width + width);
         }
+    }
+
+    return copy;
 }
 
-/*
+void _dilate_here(struct binary_image *image, struct Kernel *kernel, int x, int y) {
+   int kernel_height_index = 0;
+   int kernel_width_index = 0;
 
-void opening(binary_image image, struct Kernel *kernel) {
+   for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
+      for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
+          if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == 1)
+              *(image->matrix + height * image->width + width) = BLACK;
+
+          kernel_width_index++;
+      }
+
+      kernel_width_index = 0;
+      kernel_height_index++;
+   }
+}
+
+void _erode_here(struct binary_image *image, struct Kernel *kernel, int x, int y) {
+    int kernel_height_index = 0;
+    int kernel_width_index = 0;
+          
+    int fit = 0;
+
+    for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
+        for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
+            if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == 1) {
+                *(image->matrix + height * image->width + width) = WHITE;
+
+                fit++;
+            }
+
+            kernel_width_index++;
+        }
+
+        kernel_width_index = 0;
+        kernel_height_index++;
+    }
+
+    if (fit == kernel->height * kernel->width)
+        *(image->matrix + y * image->width + x) = BLACK;
+}
+
+void dilation(struct binary_image *image, struct Kernel *kernel) {
+    struct binary_image *image_copy = _copy_binary(image);
+
+    for (int height = 0; height < image->height; height++)
+        for (int width = 0; width < image->width; width++)
+            if (*(image_copy->matrix + height * image->width + width) == BLACK)
+                if (_check_kernel(image_copy, kernel, width, height) == HIT) 
+                    _dilate_here(image, kernel, width, height);
+}
+
+void erosion(struct binary_image *image, struct Kernel *kernel) {
+    struct binary_image *image_copy = _copy_binary(image);
+
+    for (int height = 0; height < image->height; height++)
+        for (int width = 0; width < image->width; width++)
+            if (*(image_copy->matrix + height * image->width + width) == BLACK)
+                if (_check_kernel(image_copy, kernel, width, height) != OUT)
+                    _erode_here(image, kernel, width, height);
+}
+
+void opening(struct binary_image *image, struct Kernel *kernel) {
     dilation(image, kernel);
     opening(image, kernel);
 }
 
-void closing(binary_image image, struct Kernel *kernel) {
+void closing(struct binary_image *image, struct Kernel *kernel) {
     erosion(image, kernel);
     dilation(image, kernel);
 }
-
-*/
