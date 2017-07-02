25acef5ce005b0d1cad159f6850edd674ecebcb2
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-07-02 21:15:19 +0300

    Refactor

--- src/morphology.c
+++ src/morphology.c
@@ -2,15 +2,36 @@
 #include "morphology.h"
 
 enum kernel_state _check_kernel(struct binary_image *image, struct Kernel *kernel, int x, int y) {
-    int kernel_state_count = 0;
-
     int kernel_width_index = 0;
     int kernel_height_index = 0;
 
-    for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
-        for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
-            if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == *(image->matrix + height * image->width + width))
-               kernel_state_count++;
+    int kernel_state_count = 0;
+    int kernel_size = 0;
+
+    for (int height = y - kernel->height / 2; height <= y + kernel->height / 2; height++) {
+        int _height = height;
+
+        if (height < 0)
+            _height = 0;
+
+        if (height >= image->height)
+            _height = image->height - 1;
+
+        for (int width = x - kernel->width / 2; width <= x + kernel->width / 2; width++) {
+            int _width = width;
+
+            if (width < 0)
+                _width = 0;
+
+            if (width >= image->width)
+                _width = image->width - 1;
+
+            if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == 1) {
+                kernel_size++;
+
+                if (*(image->matrix + _height * image->width + _width) == WHITE)
+                    kernel_state_count++;
+            }
 
             kernel_width_index++;
         }
@@ -19,22 +40,26 @@ enum kernel_state _check_kernel(struct binary_image *image, struct Kernel *kerne
         kernel_height_index++;
     }
 
-    if (kernel_state_count == kernel->width * kernel->height)
+    if (kernel_state_count == kernel_size)
         return FIT;
     else if (kernel_state_count == 0)
         return OUT;
     else
         return HIT;
+
 }
 
 struct Kernel *square_kernel(int size) {
-    struct Kernel *kernel = malloc(size * size);
-
-    for (int index = 0; index < size; index++)
-        kernel->kernel[index] = 1;
+    struct Kernel *kernel = malloc(sizeof(struct Kernel));
 
     kernel->width = size;
     kernel->height = size;
+
+    kernel->kernel = malloc(kernel->width * kernel->height * sizeof(char));
+
+    for (int height = 0; height < kernel->height; height++)
+        for (int width = 0; width < kernel->width; width++)
+            *(kernel->kernel + height * kernel->width + width) = 1;
 
     return kernel;
 }
@@ -60,56 +85,17 @@ struct binary_image *_copy_binary(struct binary_image *image) {
     return copy;
 }
 
-void _dilate_here(struct binary_image *image, struct Kernel *kernel, int x, int y) {
-   int kernel_height_index = 0;
-   int kernel_width_index = 0;
-
-   for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
-      for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
-          if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == 1)
-              *(image->matrix + height * image->width + width) = BLACK;
-
-          kernel_width_index++;
-      }
-
-      kernel_width_index = 0;
-      kernel_height_index++;
-   }
-}
-
-void _erode_here(struct binary_image *image, struct Kernel *kernel, int x, int y) {
-    int kernel_height_index = 0;
-    int kernel_width_index = 0;
-          
-    int fit = 0;
-
-    for (int height = y - kernel->height / 2; height >= 0 && height < y + kernel->height / 2; height++) {
-        for (int width = x - kernel->width / 2; width >= 0 && width < x + kernel->width / 2; width++) {
-            if (*(kernel->kernel + kernel_height_index * kernel->width + kernel_width_index) == 1) {
-                *(image->matrix + height * image->width + width) = WHITE;
-
-                fit++;
-            }
-
-            kernel_width_index++;
-        }
-
-        kernel_width_index = 0;
-        kernel_height_index++;
-    }
-
-    if (fit == kernel->height * kernel->width)
-        *(image->matrix + y * image->width + x) = BLACK;
-}
-
 void dilation(struct binary_image *image, struct Kernel *kernel) {
     struct binary_image *image_copy = _copy_binary(image);
 
     for (int height = 0; height < image->height; height++)
         for (int width = 0; width < image->width; width++)
-            if (*(image_copy->matrix + height * image->width + width) == BLACK)
-                if (_check_kernel(image_copy, kernel, width, height) == HIT) 
-                    _dilate_here(image, kernel, width, height);
+            if (*(image_copy->matrix + height * image->width + width) == BLACK) {
+                if (_check_kernel(image_copy, kernel, width, height) == HIT) { 
+                    *(image->matrix + height * image->width + width) = WHITE;
+                }
+            }
+
 }
 
 void erosion(struct binary_image *image, struct Kernel *kernel) {
@@ -117,14 +103,16 @@ void erosion(struct binary_image *image, struct Kernel *kernel) {
 
     for (int height = 0; height < image->height; height++)
         for (int width = 0; width < image->width; width++)
-            if (*(image_copy->matrix + height * image->width + width) == BLACK)
-                if (_check_kernel(image_copy, kernel, width, height) != OUT)
-                    _erode_here(image, kernel, width, height);
+            if (*(image_copy->matrix + height * image->width + width) == WHITE) {
+                if (_check_kernel(image_copy, kernel, width, height) != FIT) {
+                    *(image->matrix + height * image->width + width) = BLACK;
+                }
+            }
 }
 
 void opening(struct binary_image *image, struct Kernel *kernel) {
     dilation(image, kernel);
-    opening(image, kernel);
+    erosion(image, kernel);
 }
 
 void closing(struct binary_image *image, struct Kernel *kernel) {
