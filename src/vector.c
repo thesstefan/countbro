81b64998e00ac53e9ea88b8c4548f890bc411e0b
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-08-10 19:21:14 +0300

    Fixed vector problems

--- src/vector.c
+++ src/vector.c
@@ -4,16 +4,16 @@
 #include "vector.h"
 
 struct Vector *vector_create() {
-    struct Vector *new_vector = malloc(sizeof(vector));
+    struct Vector *new_vector = malloc(sizeof(new_vector));
 
     new_vector->capacity = VECTOR_INIT_CAPACITY;
-    new_vector->count = 0;
+    new_vector->size = 0;
     new_vector->items = malloc(sizeof(int) * new_vector->capacity);
 
     return new_vector;
 }
 
-struct Vector *vector_resize(struct Vector *vector, int capacity) {
+void vector_resize(struct Vector *vector, int capacity) {
     #ifdef DEBUG_ON
         printf("Vector resize : %d to %d\n", vector->capacity, capacity);
     #endif
@@ -27,23 +27,31 @@ struct Vector *vector_resize(struct Vector *vector, int capacity) {
 }
 
 void vector_add(struct Vector *vector, int item) {
-    if (vector->capacity = vector->count)
-        vector = vector_resize(vector, vector->capacity * 2);
+    if (vector->capacity == vector->size)
+        vector_resize(vector, vector->capacity * 2);
 
-    vector->count++;
+    vector->items[vector->size] = item;
 
-    vector->items[vector->count] = item;
+    #ifdef DEBUG_ON
+        printf("Added %d at index %d\n", item, vector->size);
+    #endif
+
+    vector->size += 1;
 }
 
 void vector_set(struct Vector *vector, int index, int item) {
-    if (index >= 0 && index < vector->count)
-       v->items[index] = item;
-    else  
+    if (index >= 0 && index < vector->size) {
+        vector->items[index] = item; 
+
+        #ifdef DEBUG_ON
+            printf("Element at index %d was set with the value %d", index, item);
+        #endif 
+    } else  
        fprintf(stderr, "vector_set() ERROR : Element %d does not exist", index);
 }
 
 int vector_get(struct Vector *vector, int index) {
-    if (index >= 0 && index < vector->count)
+    if (index >= 0 && index < vector->size)
         return vector->items[index];
     else {
         fprintf(stderr, "vector_get() ERROR : Element %d does not exist", index);
@@ -53,22 +61,28 @@ int vector_get(struct Vector *vector, int index) {
 }
 
 void vector_delete(struct Vector *vector, int index) {
-    if (index >= 0 && index < vector->count) {
-        for (int shift_index = index; shift_index < vector->count - 1; shift_index++)
+    if (index >= 0 && index < vector->size) {
+        for (int shift_index = index; shift_index < vector->size - 1; shift_index++)
             vector->items[shift_index] = vector->items[shift_index + 1];
 
-        vector->items[vector_count - 1] = NULL;
-        vector->count--;
+        vector->items[vector->size - 1] = -1;
+        vector->size--;
 
-        if (vector->count > 0 && vector->count == vector->capacity / 4)
-            vector->resize(vector, vector->capacity / 2);
+        if (vector->size > 0 && vector->size == vector->capacity / 4)
+            vector_resize(vector, vector->capacity / 2);
+
+        #ifdef DEBUG_ON
+            printf("Deleted the element at index %d\n", index);
+        #endif
     } else {
         fprintf(stderr, "vector_remove() ERROR : Element %d does not exist", index);
-
-        return -1;
     }
 }
 
 void vector_free(struct Vector *vector) {
-    free(vector_items);
+    free(vector->items);
+
+    #ifdef DEBUG_ON
+        printf("The vector was deleted\n");
+    #endif
 }
