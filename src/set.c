22d185393c24f02b504e2f2f010d5e62fe6d889d
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-08-17 20:52:49 +0300

    Fix if-else problem. Change set names from set_1 and set_2 to a and b

--- src/set.c
+++ src/set.c
@@ -26,70 +26,69 @@ int set_find(struct Set *set, int item) {
 }
 
 int set_add(struct Set *set, int item) {
-    if (set_find(set, item) == 0) {
-        vector_add(set->representation, item);
-
-        return 1;
-    } else
+    if (set_find(set, item) == 1)
         return 0;
+
+    vector_add(set->representation, item);
+
+    return 1;
 }
 
 int set_remove(struct Set *set, int item) {
-    if (set_find(set, item))
-        for (int index = 0; index < set->representation->size; index++)
-            if (item == vector_get(set->representation, index)) {
-                vector_remove(set->representation, index);
+    if (set_find(set, item) == 0)
+        return 0;
 
-                return 1;
-            }
+    for (int index = 0; index < set->representation->size; index++)
+        if (item == vector_get(set->representation, index)) {
+            vector_remove(set->representation, index);
 
-    return 0;
+            return 1;
+        }
 }
 
 int set_cardinal(struct Set *set) {
     return set->representation->size;
 }
 
-int set_equals(struct Set *set_1, struct Set *set_2) {
-    if (set_1->representation->size != set_2->representation->size) {
+int set_equals(struct Set *a, struct Set *b) {
+    if (a->representation->size != b->representation->size)
         return 0;
-    } else {
-        for (int index = 0; index < set_1->representation->size; index++)
-            if (set_find(set_2, vector_get(set_1->representation, index)) == 0)
+        
+    for (int index = 0; index < a->representation->size; index++)
+        if (set_find(b, vector_get(a->representation, index)) == 0)
                 return 0;
 
-        return 1;
-    }
+    return 1;
 }
 
-struct Set *set_intersection(struct Set *set_1, struct Set *set_2) {
+struct Set *set_intersection(struct Set *a, struct Set *b) {
     struct Set *intersection = set_create();
 
-    for (int index = 0; index < set_1->representation->size; index++)
-        if (set_find(set_2, vector_get(set_1->representation, index) == 1))
-            set_add(intersection, vector_get(set_1->representation, index));
+    for (int index = 0; index < a->representation->size; index++)
+        if (set_find(b, vector_get(a->representation, index) == 1))
+            set_add(intersection, vector_get(a->representation, index));
 
     return intersection;
 }
 
-struct Set *set_union(struct Set *set_1, struct Set *set_2) {
+struct Set *set_union(struct Set *a, struct Set *b) {
     struct Set *union_set = set_create();
 
-    for (int index = 0; index < set_1->representation->size; index++)
-        set_add(union_set, vector_get(set_1->representation, index));
+    for (int index = 0; index < a->representation->size; index++)
+        set_add(union_set, vector_get(a->representation, index));
 
-    for (int index = 0; index < set_2->representation->size; index++)
-        set_add(union_set, vector_get(set_2->representation, index));
+    for (int index = 0; index < b->representation->size; index++)
+        set_add(union_set, vector_get(b->representation, index));
 
     return union_set;
 }
 
-struct Set *set_difference(struct Set *set_1, struct Set *set_2) {
+struct Set *set_difference(struct Set *a, struct Set *b) {
     struct Set *difference = set_create();
 
-    for (int index = 0; index < set_1->representation->size; index++)
-        if (set_find(set_2, vector_get(set_1->representation, index) == 0))
-            set_add(difference, vector_get(set_1->representation, index));
+    for (int index = 0; index < a->representation->size; index++)
+        if (set_find(b, vector_get(a->representation, index) == 0))
+            set_add(difference, vector_get(a->representation, index));
 
     return difference;
 }
