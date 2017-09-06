bbbad4ebd01988eb2b0edb637105483c3b15c730
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-09-06 12:52:35 +0300

    Refact the vector test, respecting SRP

--- test_src/vector_test.c
+++ test_src/vector_test.c
@@ -3,56 +3,108 @@
 
 #include "vector.h"
 
+int vector_create_test() {
+    printf("vector_create() test -> ");
+
+    struct Vector *vector = vector_create();
+
+    if (vector == NULL || vector->size != 0 || vector->capacity != VECTOR_INIT_CAPACITY || vector->items == NULL) {
+        printf("ERRROR");
+        return 0;
+    }
+
+    return 1;
+}
+
+int vector_add_test() {
+    printf("vector_add() test -> ");
+
+    struct Vector *vector = vector_create();
+
+    for (int index = 0; index < 100; index++)
+        if (vector_add(vector, index) == 0)
+            return 0;
+
+    if (vector->size != 100 || vector->capacity < 100)
+        return 0;
+
+    return 1;
+}
+
+int vector_get_test() {
+    printf("vector_get() test -> ");
+
+    struct Vector *vector = vector_create();
+
+    for (int index = 0; index < 100; index++) {
+        vector_add(vector, index);
+
+        if (vector_get(vector, index) != index && index >= 0 && index < vector->size)
+            return 0;
+    }
+
+    return 1;
+}
+
+int vector_set_test() {
+    printf("vector_set() test -> ");
+
+    struct Vector *vector = vector_create();
+
+    for (int index = 0; index < 100; index++) {
+        vector_add(vector, index);
+
+        if (vector_set(vector, index, 1) == 0 && index >= 0 && index < vector->size)
+            return 0;
+
+        if (vector_get(vector, index) != 1 && index >= 0 && index < vector->size)
+            return 0;
+    }
+
+    return 1;
+}
+
+int vector_remove_test() {
+    printf("vector_remove() test -> ");
+
+    struct Vector *vector = vector_create();
+
+    for (int index = 0; index < 100; index++) {
+        vector_add(vector, index);
+
+        if (vector_remove(vector, 0) == 0 && index >= 0 && index < vector->size) 
+            return 0;
+    }
+
+    if (vector->size != 0 || vector->capacity > 20) 
+       return 0;
+
+   return 1;
+} 
+
 int main() {
     struct Vector *vector = vector_create();
 
-    printf("vector_create() test -> ");
+    // Padding
+    printf("\n\n");
 
-    assert(vector != NULL);
-    assert(vector->size == 0);
-    assert(vector->capacity == VECTOR_INIT_CAPACITY);
-
+    assert(vector_create_test()); 
     printf("Success\n");
 
-    printf("vector_resize() test -> ");
-
+    assert(vector_add_test());
     printf("Success\n");
 
-    printf("vector_add() test -> ");
-
-    for (int index = 0; index < 100; index++)
-        assert(vector_add(vector, index));
-
-    assert(vector->size == 100);
-    assert(vector->capacity > 100);
-
-    printf("Success\n");
-    
-    printf("vector_get() test -> ");
-
-    for (int index = 0; index < 100; index++)
-        assert(vector_get(vector, index) == index);
-
+    assert(vector_get_test());
     printf("Success\n");
 
-    printf("vector_set() test -> ");
-
-    for (int index = 0; index < 100; index++) {
-        assert(vector_set(vector, index, 1) == 1);
-        assert(vector_get(vector, index) == 1);
-    }
-    
+    assert(vector_set_test());
     printf("Success\n");
 
-    printf("vector_remove() test -> ");
-
-    for (int index = 99; index > 0; index--)
-        assert(vector_remove(vector, index));
-
-    assert(vector->size == 1);
-    assert(vector->capacity < 20);
-
+    assert(vector_remove_test());
     printf("Success\n");
+
+    // Padding for the next test
+    printf("\n\n");
 
     return 0; 
 }
