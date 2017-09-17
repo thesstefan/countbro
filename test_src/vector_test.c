fd56f967a9250f015cfed1ea8b24a0195dc94d46
TheMightyStefan <stefan.1.stefanache@gmail.com>
2017-09-17 09:00:27 +0300

    Remake vector_add_test()

--- test_src/vector_test.c
+++ test_src/vector_test.c
@@ -2,6 +2,19 @@
 
 #include "vector.h"
 
+#define TEST_LIMIT 100
+
+int vector_is_equal(struct Vector *vector_1, struct Vector *vector_2) {
+    if (vector_1->size != vector_2->size)
+        return 0;
+
+    for (int index = 0; index < vector_1->size; index++)
+        if (vector_1->items[index] != vector_2->items[index])
+            return 0;
+
+    return 1;
+}
+
 int vector_create_test() {
     struct Vector *vector = vector_create();
 
@@ -11,26 +24,24 @@ int vector_create_test() {
     return 1;
 }
 
-int vector_add_test() {
+int vector_add_test(struct Vector *sample) {
     struct Vector *vector = vector_create();
 
-    for (int index = 0; index < 100; index++) {
+    for (int index = 0; index < TEST_LIMIT; index++) {
         if (vector_add(vector, index) == 0)
             return 0;
 
-        if (vector->items[vector->size - 1] != index)
+        if (vector->items[index] != index)
             return 0;
     }
 
-    if (vector->size != 100 || vector->capacity < 100)
+    if (!vector_is_equal(vector, sample))
         return 0;
 
     return 1;
 }
 
-int vector_get_test() {
-    struct Vector *vector = vector_create();
-
+int vector_get_test(struct Vector *vector) {
     for (int index = 0; index < 100; index++) {
         vector_add(vector, index);
 
@@ -44,9 +55,7 @@ int vector_get_test() {
     return 1;
 }
 
-int vector_set_test() {
-    struct Vector *vector = vector_create();
-
+int vector_set_test(struct Vector *vector) {
     for (int index = 0; index < 100; index++) {
         vector_add(vector, index);
 
@@ -60,9 +69,7 @@ int vector_set_test() {
     return 1;
 }
 
-int vector_remove_test() {
-    struct Vector *vector = vector_create();
-
+int vector_remove_test(struct Vector *vector) {
     for (int index = 0; index < 100; index++) {
         vector_add(vector, index);
 
@@ -86,16 +93,31 @@ void evaluate(int value) {
         printf("Fail\n");
 }
 
+struct Vector *create_sample() {
+    struct Vector *sample = vector_create();
+
+    sample->size = TEST_LIMIT;
+    sample->capacity = TEST_LIMIT;
+
+    for (int index = 0; index < TEST_LIMIT; index++)
+        sample->items[index] = index;
+
+    return sample;
+}
+
 int main() {
     // Padding
     printf("\n\n");
+
+    struct Vector *sample = create_sample();
 
     printf("vector_create() test -> ");
     evaluate(vector_create_test()); 
 
     printf("vector_add() test -> ");
-    evaluate(vector_add_test());
+    evaluate(vector_add_test(sample));
 
+/*
     printf("vector_get() test -> ");
     evaluate(vector_get_test());
 
@@ -107,6 +129,7 @@ int main() {
 
     // Padding for the next test
     printf("\n\n");
+*/
 
     return 0; 
 }
